ba10b2395299d20b656ee3a8ad47d611c965c8e0
Dario_Quintanilla <DarioQuintanilla2@gmail.com>
2021-07-08 21:12:19 -0400

    pop up modal dimensions set for calculator

--- App.js
+++ App.js
@@ -1,32 +1,36 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
   useTheme,
 } from '@react-navigation/native'
+import HomeScreen from './src/home/Home'
 import { createStackNavigator } from '@react-navigation/stack'
 import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
+import sourceToad from './assets/sourcetoad-loader.png'
 import DT from './darkTheme'
 import myDarkTheme from './darkTheme'
-
-function HomeScreen() {
-  return (
-    <SafeAreaView>
-      <View>
-        <Text style={{ color: 'white' }}>Home Screen</Text>
-      </View>
-    </SafeAreaView>
-  )
-}
 
 const HomeStack = createStackNavigator()
 
 function HomeScrenStack({ navigation }) {
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name='Home' component={HomeScreen}></HomeStack.Screen>
+      <HomeStack.Screen
+        name='HomeScreen'
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          title: 'SourceToad Calculator',
+          headerLeft: () => (
+            <Image
+              style={{ height: 50, width: 50, borderRadius: 100 }}
+              source={require('./assets/sourcetoad-loader.png')}
+            />
+          ),
+        })}
+      ></HomeStack.Screen>
     </HomeStack.Navigator>
   )
 }
---
+++ src/home/Home.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Modal,
+  TouchableOpacity,
+  Pressable,
+  StyleSheet,
+} from 'react-native'
+import { widthPercentageToDP, heightPercentageToDP } from '../../utils/scaler'
+
+function Home() {
+  const [modalVisible, setModalVisible] = useState(false)
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible)
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>X</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Open Calculator</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    width: '90%',
+    height: '80%',
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+    marginRight: 'auto',
+    marginBottom: 5,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+})
+
+export default Home
